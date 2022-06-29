be72a1af7844ccc6cd9befd4bbbed11208b18438
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-06-29 16:16:38 -0400

    add POJO models

---
+++ src/main/java/com/sujeevuthayakumar/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.sujeevuthayakumar.springpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/sujeevuthayakumar/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sujeevuthayakumar.springpetclinic.model;
+
+public class Vet extends Person{
+}
