54584eff4a1c770b0be43cb411c40b52b1e3e230
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-07-27 09:15:33 -0400

    add service for PetTypes

---
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.sujeevuthayakumar.springpetclinic.services;
+
+import com.sujeevuthayakumar.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
