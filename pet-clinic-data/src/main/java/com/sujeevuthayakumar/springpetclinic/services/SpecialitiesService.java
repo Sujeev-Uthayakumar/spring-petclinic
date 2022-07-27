a243f85d20bb9dcc880aee878e10b222ac841ec4
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-07-27 09:43:43 -0400

    add specialities to vet service

---
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.sujeevuthayakumar.springpetclinic.services;
+
+import com.sujeevuthayakumar.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
