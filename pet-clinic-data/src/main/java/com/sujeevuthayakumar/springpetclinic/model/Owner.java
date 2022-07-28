64ad3156dadaa9f62c663d718d9ad8d0b78a791d
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-07-28 17:28:59 -0400

    add updated JPA entity mappings

--- pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package com.sujeevuthayakumar.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "pets")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
     public Set<Pet> getPets() {
