19541e691ef19308600ff1d949d08641242772ca
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-08-02 08:38:32 -0400

    refactor to use project lombok

--- pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/model/Owner.java
@@ -1,12 +1,19 @@
 package com.sujeevuthayakumar.springpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "owners")
-public class Owner extends Person{
+public class Owner extends Person {
 
     @Column(name = "pets")
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
@@ -21,35 +28,4 @@ public class Owner extends Person{
     @Column(name = "telephone")
     private String telephone;
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
