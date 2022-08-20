d068210ff54d64a82d90ff02b88c66e600d76ae7
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2022-08-19 21:39:28 -0400

    add find Owners controller

--- pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,11 @@ package com.sujeevuthayakumar.springpetclinic.repositories;
 import com.sujeevuthayakumar.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/sujeevuthayakumar/springpetclinic/services/OwnerService.java
@@ -2,6 +2,11 @@ package com.sujeevuthayakumar.springpetclinic.services;
 
 import com.sujeevuthayakumar.springpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
+
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
